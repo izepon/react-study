f472099fc6a7ea1e2092f3d74c8255f136bcf45f
Jean Izepon <izeponjc@gmail.com>
2022-01-02 21:07:07 -0300

    create: project react study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "Jean Carlos Izepon",
+    counter: 0,
+  };
+
+  handlePClick = () => {
+    this.setState({ name: "Izepon" });
+  };
+
+  handleAClick = (event) => {
+    //Para buscar o "this" desse click, usei um hack do javascript que é arrow function que não tem this, sendo assim vai buscar o elemento do pai que é a class App e assim vamos ter a mesma execução de um "this" sem precisar fazer o bind.
+    event.preventDefault(); // previne eventos.
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Olá Mundo!</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
