026ea559c03fe31cc6eca5070d6aba2ffc3e075b
Jean Izepon <izeponjc@gmail.com>
2022-01-04 21:35:04 -0300

    update: criado estrutura Data-Fetch e CSS da página com posts

--- src/App.js
+++ src/App.js
@@ -6,20 +6,44 @@ class App extends Component {
     posts: [],
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url };
+    });
+
+    this.setState({ posts: postsAndPhotos });
+    console.log(postsAndPhotos);
+  };
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
